800f9b20c714caeacf0e0329e8ecf86cd942ef40
Hikaru NASHIDA <nashida@xcoo.jp>
2019-06-03 12:02:49 +0900

    Add the accordion menu markup

--- src/cljs/accordion_menu/views.cljs
+++ src/cljs/accordion_menu/views.cljs
@@ -2,7 +2,27 @@
   (:require [re-frame.core :as re-frame]
             [accordion-menu.subs :as subs]))
 
-(defn main-panel []
+#_(defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
      [:h1 "Hello from " @name]]))
+
+(defn content-list [num more-num]
+  [:li (str num "x" more-num " = " (* num more-num))])
+
+(defn accordion-content [num]
+  [:ul.acc-content
+   (map #(content-list num %) (range 1 10))])
+
+(defn list-accordion [num]
+  [:div {:id num
+         :key num}
+   [:input.acc-toggle {:type "checkbox"
+                       :id (str "toggle_" num)}]
+   [:label.acc-label {:for (str "toggle_" num)}
+    [:span (str num "の段")]]
+   [accordion-content num]])
+
+(defn main-panel []
+  [:div.accbox
+   (doall (map list-accordion (range 1 10)))])
