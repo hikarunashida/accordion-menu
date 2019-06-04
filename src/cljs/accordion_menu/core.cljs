341c6256c1c76adc309f02631f77523dc7bddf69
Hikaru NASHIDA <nashida@xcoo.jp>
2019-06-04 11:24:03 +0900

    Move the view section to core.cljs

--- src/cljs/accordion_menu/core.cljs
+++ src/cljs/accordion_menu/core.cljs
@@ -1,6 +1,28 @@
 (ns accordion-menu.core
-  (:require [reagent.core :as reagent]
-            [accordion-menu.views :as views]))
+  (:require [reagent.core :as reagent]))
+
+;; view
+
+(defn content-list [num more-num]
+  [:li {:key (str num "x" more-num)}
+   (str num "x" more-num " = " (* num more-num))])
+
+(defn list-accordion [num]
+  [:div {:id num
+         :key num}
+   [:input.acc-toggle {:type "checkbox"
+                       :id (str "toggle_" num)}]
+   [:label.acc-label {:for (str "toggle_" num)}
+    [:span (str num "の段")]]
+   [:div.acc-content
+    [:ul
+     (map #(content-list num %) (range 1 10))]]])
+
+;; initialization
+
+(defn main-panel []
+  [:div.accbox
+   (doall (map list-accordion (range 1 10)))])
 
 (defn dev-setup []
   (when ^boolean goog.DEBUG
@@ -8,7 +30,7 @@
     (println "dev mode")))
 
 (defn mount-root []
-  (reagent/render [views/main-panel]
+  (reagent/render [main-panel]
                   (.getElementById js/document "app")))
 
 (defn ^:export init []
